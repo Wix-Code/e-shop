85762de10a5c4abc6b2bb1bc0386d842c841eed0
Wix-Code <ogbonna428alex@gmail.com>
2025-02-05 06:01:49 +0100

    push

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,8 +1,20 @@
-export const createProduct = async () => {
+import Product from "../models/product.model";
+
+export const createProduct = async (req, res) => {
+  const { userId, title, description, cat, brand } = req.body;
+
+  if (!userId ||!title ||!description ||!cat ||!brand) {
+    return res.json(400).json({ success: false, message: "All fields are required" });
+  }
   try {
+
+    const product = await Product.create({ userId, title, description, cat, brand });
+    await product.save();
+    res.json({ success: true, message: "Product created successfully", product });
     
   } catch (error) {
-    
+    res.json({ success: false, message: "Product not created successfully" });
+    console.log(error);
   }
 }
 
