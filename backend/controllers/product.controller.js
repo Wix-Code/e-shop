aac4ced3db56e02bdc7efb32030cf2691717ae6b
Wix-Code <ogbonna428alex@gmail.com>
2025-02-05 06:08:29 +0100

    push

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,28 +1,30 @@
 import Product from "../models/product.model";
 
 export const createProduct = async (req, res) => {
-  const { userId, title, description, cat, brand } = req.body;
+  const { userId, title, description, cat, brand, img, inStock, price } = req.body;
 
-  if (!userId ||!title ||!description ||!cat ||!brand) {
-    return res.json(400).json({ success: false, message: "All fields are required" });
+  if (!userId ||!title ||!description ||!cat ||!brand, !img, !inStock, !price) {
+    return res.status(400).json({ success: false, message: "All fields are required" });
   }
   try {
 
-    const product = await Product.create({ userId, title, description, cat, brand });
+    const product = await Product.create({ userId, title, description, cat, brand, img, inStock, price });
     await product.save();
-    res.json({ success: true, message: "Product created successfully", product });
+    res.status(400).json({ success: true, message: "Product created successfully", product });
     
   } catch (error) {
-    res.json({ success: false, message: "Product not created successfully" });
+    res.status(500).json({ success: false, message: "Product not created successfully" });
     console.log(error);
   }
 }
 
 export const getAllProducts = async () => {
   try {
-    
+    const product = await Product.find({});
+    res.status(200).json({success: true, message: "Products Fetched", product})
   } catch (error) {
-    
+    console.log(error);
+    res.status(500).json({success: false, message: "Failed to fetch products"})
   }
 }
 
