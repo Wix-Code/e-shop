8f077a72a40cea2552ecd4368ea6a1ad18566aeb
Wix-Code <ogbonna428alex@gmail.com>
2025-02-08 23:38:07 +0100

    push

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -20,7 +20,7 @@ export const createProduct = async (req, res) => {
 
 export const getAllProducts = async (req,res) => {
   try {
-    const product = await Product.find({}).populate("comment");
+    const product = await Product.find({});
     res.status(200).json({success: true, message: "Products Fetched", product})
   } catch (error) {
     console.log(error);
@@ -31,7 +31,7 @@ export const getAllProducts = async (req,res) => {
 export const getSingleproduct = async (req, res) => {
   const { id } = req.params;
   try {
-    const product = await Product.findById(id);
+    const product = await Product.findById(id).populate("comment");
     if (!product) {
       return res.status(404).json({ success: false, message: "Product not found" });
     }
