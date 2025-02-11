ef43f701e92aa0aba13955e345c3c7c501c77d76
Wix-Code <ogbonna428alex@gmail.com>
2025-02-11 01:59:02 +0100

    push

--- backend/routes/wishlist.route.js
+++ backend/routes/wishlist.route.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { deleteProductFromWishlist, wishlist } from "../controllers/wishlist.controller.js";
+import { deleteProductFromWishlist, getWishlist, wishlist } from "../controllers/wishlist.controller.js";
 import { VerifyToken } from "../lib/VerifyToken.js";
 
 const router = express.Router()
 
 router.post("/", VerifyToken, wishlist)
 router.post("/delete", VerifyToken, deleteProductFromWishlist)
+router.post("/", VerifyToken, getWishlist)
 
 export default router;
