fc7d654de1aa7ecb043e9197946f3bca31c3286f
Wix-Code <ogbonna428alex@gmail.com>
2025-02-15 14:58:41 +0100

    push

--- frontend/src/subpages/ResponsiveNavbar.jsx
+++ frontend/src/subpages/ResponsiveNavbar.jsx
@@ -4,22 +4,32 @@ import { FiHome } from "react-icons/fi";
 import { BiSolidCategory } from "react-icons/bi";
 import { FaShoppingCart } from 'react-icons/fa'
 import { MdAccountCircle } from 'react-icons/md';
+import { Link, useLocation } from "react-router-dom"
 
 const ResponsiveNavbar = () => {
+  const location = useLocation()
+  const pathname = location.pathname
+
   return (
     <div className='responsive'>
-      <div className="respon">
-        <FiHome />
-        <p>Home</p>
-      </div>
-      <div className="respon">
-        <BiSolidCategory />
-        <p>Categories</p>
-      </div>
-      <div className="respon">
-        <FaShoppingCart />
-        <p>Cart</p>
-      </div>
+      <Link to="/">
+        <div className={pathname === "/" ? "active" : "respon"}>
+          <FiHome />
+          <p>Home</p>
+        </div>
+      </Link>
+      <Link to="/store">
+        <div className={pathname === "/store" ? "active" : "respon"}>
+          <BiSolidCategory />
+          <p>Categories</p>
+        </div>
+      </Link>
+      <Link to="/cart">
+        <div className={pathname === "/cart" ? "active" : "respon"}>
+          <FaShoppingCart />
+          <p>Cart</p>
+        </div>
+      </Link>
       <div className="respon">
         <MdAccountCircle />
         <p>My Accounts</p>
