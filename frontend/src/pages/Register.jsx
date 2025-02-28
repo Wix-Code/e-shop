a8d028d0b4a90e070cfae542cbe9c8ed6a696f8b
Wix-Code <ogbonna428alex@gmail.com>
2025-02-28 09:22:39 +0100

    push

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Formik } from "formik"
 import "./auth.css"
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <Formik>
+      <form action="">
+        <h2>Create an account.</h2>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" name="username" />
+        </div>
+        <div>
+          <label htmlFor="username">Email</label>
+          <input type="text" id="username" name="username" />
+        </div>
+        <div>
+          <label htmlFor="username">Password</label>
+          <input type="text" id="username" name="username" />
+        </div>
+      </form>
+    </Formik>
   )
 }
 
